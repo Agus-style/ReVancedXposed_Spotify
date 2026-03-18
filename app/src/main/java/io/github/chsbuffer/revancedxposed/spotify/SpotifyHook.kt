81ef79d169c00586f9e18e517d7a1a4eb8e75499
Agus-Style <96540692+Agus-style@users.noreply.github.com>
2026-03-19 00:20:12 +0700

    Update SpotifyHook.kt

--- app/src/main/java/io/github/chsbuffer/revancedxposed/spotify/SpotifyHook.kt
+++ app/src/main/java/io/github/chsbuffer/revancedxposed/spotify/SpotifyHook.kt
@@ -4,6 +4,8 @@ import android.app.Application
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam
 import io.github.chsbuffer.revancedxposed.BaseHook
 import io.github.chsbuffer.revancedxposed.injectHostClassLoaderToSelf
+import io.github.chsbuffer.revancedxposed.spotify.misc.DisableAds
+import io.github.chsbuffer.revancedxposed.spotify.misc.DisableForceUpdate
 import io.github.chsbuffer.revancedxposed.spotify.misc.UnlockPremium
 import io.github.chsbuffer.revancedxposed.spotify.misc.privacy.SanitizeSharingLinks
 import io.github.chsbuffer.revancedxposed.spotify.misc.widgets.FixThirdPartyLaunchersWidgets
@@ -14,12 +16,12 @@ class SpotifyHook(app: Application, lpparam: LoadPackageParam) : BaseHook(app, l
         ::Extension,
         ::SanitizeSharingLinks,
         ::UnlockPremium,
-        ::FixThirdPartyLaunchersWidgets
+        ::FixThirdPartyLaunchersWidgets,
+        ::DisableForceUpdate,
+        ::DisableAds
     )
 
     fun Extension() {
-        // load stubbed spotify classes
         injectHostClassLoaderToSelf(this::class.java.classLoader!!, classLoader)
     }
 }
-
